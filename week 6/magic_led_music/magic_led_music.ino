751b708b972a186947b607dc9a837f6608a097fb
adams012 <adams012@newschool.edu>
2016-12-11 02:33:46 -0500

    LED MUSIC
    https://vimeo.com/195161307
    
    Here is a video of my final output.

---
+++ week 6/magic_led_music/magic_led_music.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+// FOR LED, WENT INTO STANDARD FIRMATA, THEN LAUNCHED PROCESSING CODE. 
+//THERE WAS A VERY SLIGHT LAG DUE TO THE AMOUNT OF LEDS IN THE CIRCUIT.
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
